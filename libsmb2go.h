e1c31d25cbee08e88ed67a80b7e7959862b86788
Peter Sarkozi <xmisterhu@gmail.com>
2021-06-12 22:32:23 +0200

    Use proper include path

--- libsmb2go.h
+++ libsmb2go.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <unistd.h>
-#include <smb2.h>
-#include <libsmb2.h>
+#include <smb2/smb2.h>
+#include <smb2/libsmb2.h>
 
 int smb2_read_wrapper(struct smb2_context *smb2, struct smb2fh *fh, void *buf, unsigned long count, long long offset);
 
